58e693751abd1dcddce21958a1b53be5f6e0d0b9
Sergi <sergiobiolsolcina@gmail.com>
2021-04-24 22:44:17 +0200

    get family members added family info

--- src/main/java/obi/sergi/homefood/Controllers/Family/FamilyMemberController.java
+++ src/main/java/obi/sergi/homefood/Controllers/Family/FamilyMemberController.java
@@ -1,11 +1,9 @@
 package obi.sergi.homefood.Controllers.Family;
 
-import obi.sergi.homefood.Entities.Family.FamilyMember;
-import obi.sergi.homefood.Entities.Family.FamilyMemberDetailed;
-import obi.sergi.homefood.Entities.Family.FamilyMemberRegister;
-import obi.sergi.homefood.Entities.Family.FamilyRegister;
+import obi.sergi.homefood.Entities.Family.*;
 import obi.sergi.homefood.Entities.User.User;
 import obi.sergi.homefood.Repositories.Family.FamilyMemberRepository;
+import obi.sergi.homefood.Repositories.Family.FamilyRepository;
 import obi.sergi.homefood.Repositories.User.UserRepository;
 import obi.sergi.homefood.Utils.DateFormater;
 import obi.sergi.homefood.Utils.Response;
@@ -15,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -25,10 +25,15 @@ public class FamilyMemberController {
     private FamilyMemberRepository familyMemberRepository;
 
     @Autowired
+    private FamilyRepository familyRepository;
+
+    @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/userId/{userId}")
     public ResponseEntity getFamilyMembers(@PathVariable String userId) {
+        Map<Object, Object> model = new HashMap<>();
+
         FamilyMember familyMember = familyMemberRepository.findByUserId(userId);
 
         ArrayList<FamilyMemberDetailed> familyMembersDetailed = new ArrayList<>();
@@ -47,6 +52,13 @@ public class FamilyMemberController {
             }
         }
 
-        return new ResponseEntity(familyMembersDetailed, HttpStatus.valueOf(200));
+        String familyId = familyMember.getFamilyId();
+        Family family = familyRepository.findFamilyById(familyId);
+
+        model.put("familyId", family.getId());
+        model.put("familyName", family.getName());
+        model.put("familyMembersDetailed",familyMembersDetailed);
+
+        return new ResponseEntity(model, HttpStatus.valueOf(200));
     }
 }
--- src/main/java/obi/sergi/homefood/Repositories/Family/FamilyMemberRepository.java
+++ src/main/java/obi/sergi/homefood/Repositories/Family/FamilyMemberRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface FamilyMemberRepository extends MongoRepository<FamilyMember, String> {
     @org.springframework.data.mongodb.repository.Query(value = "{ 'familyMembers': { $elemMatch: { 'userId' : ?0 } }}")
     FamilyMember findByUserId(String userId);
-
 }
